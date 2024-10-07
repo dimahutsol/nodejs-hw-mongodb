3793709f235f61e854c7f898a8c44152dd470452
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-10-07 22:03:22 +0100

    add full homework

---
+++ src/server.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import * as contactServices from './services/contacts.js';
+
+import { env } from './utils/env.js';
+
+const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+  },
+});
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(logger);
+
+  app.get('/contacts', async (req, res) => {
+    const data = await contactServices.getContacts();
+
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const data = await contactServices.getContactById(contactId);
+
+    if (!data) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data,
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: `Not found`,
+    });
+  });
+
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: error.message,
+    });
+  });
+
+  const port = Number(env('PORT', 3000));
+
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+};
