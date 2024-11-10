76f607d4c210f4078b20d19cfe4b7c053ba06b9f
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-11-10 22:25:35 +0000

    add full task

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
 
 import { env } from './utils/env.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
   const app = express();
@@ -19,6 +20,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cookieParser());
   app.use(express.static('uploads'));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
